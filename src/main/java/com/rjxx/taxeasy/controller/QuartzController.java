0e23452120d23fa1f7e20e9ad153b05bad534ebf
DESKTOP-JDQTMV1\xlm <1084854604@qq.com>
2017-03-16 15:32:22 +0800

    定时任务

--- src/main/java/com/rjxx/taxeasy/controller/QuartzController.java
+++ src/main/java/com/rjxx/taxeasy/controller/QuartzController.java
@@ -1,8 +1,6 @@
 package com.rjxx.taxeasy.controller;
 
 
-import com.alibaba.fastjson.JSON;
-import com.rjxx.taxeasy.config.ResultInfo;
 import com.rjxx.taxeasy.config.ServiceException;
 import com.rjxx.taxeasy.domains.TaskInfo;
 import com.rjxx.taxeasy.service.TaskService;
@@ -11,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
