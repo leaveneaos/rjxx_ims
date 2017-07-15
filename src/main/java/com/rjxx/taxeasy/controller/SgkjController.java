63c95b97ffd69392e6b98aea4c0267e884876811
许黎明 <1084854604@qq.com>
2017-07-15 12:38:18 +0800

    修改购方税号的判断

--- src/main/java/com/rjxx/taxeasy/controller/SgkjController.java
+++ src/main/java/com/rjxx/taxeasy/controller/SgkjController.java
@@ -347,13 +347,13 @@ public class SgkjController extends BaseController{
                 msgg = "专票购方地址及电话都为空，请重新填写！";
                 msg += msgg;
             }
-            if (jyxxsq.getGfsh() != null && (jyxxsq.getGfsh().length() != 15 ||jyxxsq.getGfsh().length() !=18|| jyxxsq.getGfsh().length() !=20)) { // 购方税号长度的判断
+            if (jyxxsq.getGfsh() != null && (jyxxsq.getGfsh().length() != 15 &&jyxxsq.getGfsh().length() !=18&&jyxxsq.getGfsh().length() !=20)) { // 购方税号长度的判断
                 msgg = "购方税号不是15位到20位，请重新填写！";
                 msg += msgg;
             }
         } else {
             if(sfbx.equals("1")){
-                if (jyxxsq.getGfsh() != null && (jyxxsq.getGfsh().length() != 15 ||jyxxsq.getGfsh().length() !=18|| jyxxsq.getGfsh().length() !=20)) { // 购方税号长度的判断
+                if (jyxxsq.getGfsh() != null && (jyxxsq.getGfsh().length() != 15 &&jyxxsq.getGfsh().length() !=18&& jyxxsq.getGfsh().length() !=20)) { // 购方税号长度的判断
                     msgg = "购方税号不是15位到20位，请重新填写！";
                     msg += msgg;
                 }
