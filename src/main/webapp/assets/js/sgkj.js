4df6b242313427e6dc5fafc438d7da2c67c564b1
许黎明 <1084854604@qq.com>
2017-07-18 20:50:16 +0800

    修改单价精度

--- src/main/webapp/assets/js/sgkj.js
+++ src/main/webapp/assets/js/sgkj.js
@@ -226,7 +226,7 @@ $(function() {
             }else if(i==4){
                 $(cell).find('input[name="spsl"]').val("");
             }else if(i==5){
-                $(cell).find('input[name="spdj"]').val(data.spdj == null ? '' : FormatFloat(data.spdj, "#.00#############"));
+                $(cell).find('input[name="spdj"]').val(data.spdj == null ? '' : FormatFloat(data.spdj, "#.00####"));
             }else if(i==6){
                 $(cell).find('input[name="spje"]').val("");
             }else if(i==7){
@@ -337,9 +337,9 @@ $(function() {
         var hjjestr=0;
         var hjsestr=0;
         if (spdj.val()!= "") {
-            spsl.val(FormatFloat(spje.val() / spdj.val(), "#.00#############"));
+            spsl.val(FormatFloat(spje.val() / spdj.val(), "#.00####"));
         }else if(spdj.val()==""&&spsl.val()!=""){
-            spdj.val(FormatFloat(spje.val() / spsl.val(), "#.00#############"));
+            spdj.val(FormatFloat(spje.val() / spsl.val(), "#.00####"));
         }
         $("#jyspmx_table").find("tr").each(function(i,row){
             if(i!=0){
