2ec1299bfe777d1c9a1e0a56ee4e803fa704aa8c
许黎明 <1084854604@qq.com>
2017-08-08 18:21:01 +0800

    修改迁移数据

--- src/main/java/com/rjxx/taxeasy/controller/QyDataController.java
+++ src/main/java/com/rjxx/taxeasy/controller/QyDataController.java
@@ -39,7 +39,7 @@ public class QyDataController extends BaseController {
     public Map transferdata(){
         Map result=new HashMap();
         String gsdm=getGsdm();
-        List<Object> jylslist=Transferdata.getdata("t_jyls",gsdm,0,0);
+        List<Object> jylslist=Transferdata.getdata("t_jyls",gsdm,0,0,0,0);
         List<Jyls> jylsList=new ArrayList<>();
         List<Xf> xfList=getXfList();
         List<Skp> skpList=getSkpList();
@@ -57,8 +57,8 @@ public class QyDataController extends BaseController {
             jylsList.add(jyls);
         }
             for(Jyls jyls:jylsList){
-                List<Object> jyspmxlist=Transferdata.getdata("t_jyspmx",gsdm,jyls.getDjh(),0);
-                List<Object> kplslist=Transferdata.getdata("t_kpls",gsdm,jyls.getDjh(),0);
+                List<Object> jyspmxlist=Transferdata.getdata("t_jyspmx",gsdm,jyls.getDjh(),0,0,0);
+                List<Object> kplslist=Transferdata.getdata("t_kpls",gsdm,jyls.getDjh(),0,0,0);
                 jyls.setXfid(xfList.get(0).getId());
                 jyls.setSkpid(skpList.get(0).getId());
                 jyls.setDjh(null);
@@ -71,7 +71,7 @@ public class QyDataController extends BaseController {
                 }
                 List<Kpls> kplsList=reload(kplslist);
                 for(Kpls kpls:kplsList){
-                    List<Object> kpspmxlist=Transferdata.getdata("t_kpspmx",gsdm,0,kpls.getKplsh());
+                    List<Object> kpspmxlist=Transferdata.getdata("t_kpspmx",gsdm,0,kpls.getKplsh(),0,0);
                     kpls.setDjh(jyls.getDjh());
                     kpls.setXfid(xfList.get(0).getId());
                     kpls.setSkpid(skpList.get(0).getId());
@@ -135,5 +135,40 @@ public class QyDataController extends BaseController {
             kpspmxList.add(kpspmx);
         }
         return kpspmxList;
+    }
+    public List<Xf> reload3(List<Object> xflist){
+
+        List<Xf> XfList=new ArrayList<>();
+        for(int i=0;i<xflist.size();i++) {
+            Xf Xf=(Xf)xflist.get(i);
+            for (int j = i + 1; j < xflist.size(); j++)
+            {
+                Xf xfj=(Xf)xflist.get(j);
+                if (Xf.getId()==(xfj.getId()))
+                {
+                    System.out.println("第" + (i + 1) + "个跟第" + (j + 1) + "个重复，值是：" + Xf.getId());
+                }
+            }
+            XfList.add(Xf);
+        }
+        return XfList;
+    }
+
+    public List<Skp> reload4(List<Object> skplist){
+
+        List<Skp> SkpList=new ArrayList<>();
+        for(int i=0;i<skplist.size();i++) {
+            Skp Skp=(Skp)skplist.get(i);
+            for (int j = i + 1; j < skplist.size(); j++)
+            {
+                Skp skpj=(Skp)skplist.get(j);
+                if (Skp.getId()==(skpj.getId()))
+                {
+                    System.out.println("第" + (i + 1) + "个跟第" + (j + 1) + "个重复，值是：" + Skp.getId());
+                }
+            }
+            SkpList.add(Skp);
+        }
+        return SkpList;
     }
 }
