10422ce6d60225e14d53cf161044449407b7a86e
许黎明 <1084854604@qq.com>
2017-08-28 17:45:42 +0800

    no message

--- src/main/java/com/rjxx/taxeasy/configuration/ServiceException.java
+++ src/main/java/com/rjxx/taxeasy/configuration/ServiceException.java
@@ -4,6 +4,7 @@ package com.rjxx.taxeasy.configuration;
  * Created by xlm on 2017/3/14.
  */
 public class ServiceException extends RuntimeException{
+    private static final long serialVersionUID = 8624944628363400977L;
 
     public ServiceException() {
         super();
