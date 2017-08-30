c05431051f8a2bb017289705de64f60970485b19
kangzhongxu <kangzhongxu123>
2017-08-30 10:16:28 +0800

    绿地平台提取开票

---
+++ src/main/java/com/rjxx/taxeasy/controller/PttqkpController.java
@@ -0,0 +1,191 @@
+package com.rjxx.taxeasy.controller;
+
+import com.rjxx.comm.mybatis.Pagination;
+import com.rjxx.taxeasy.bizcomm.utils.FpclService;
+import com.rjxx.taxeasy.bizcomm.utils.GetDataService;
+import com.rjxx.taxeasy.domains.*;
+import com.rjxx.taxeasy.filter.SystemControllerLog;
+import com.rjxx.taxeasy.service.*;
+import com.rjxx.taxeasy.vo.Spvo;
+import com.rjxx.taxeasy.web.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/pttqkp")
+public class PttqkpController extends BaseController {
+
+	@Autowired
+	private SmService smService;
+	@Autowired
+	private CszbService cszbService;
+	@Autowired
+	private SpvoService spvoService;
+	@Autowired
+	private SpzService spzService;
+	@Autowired
+	private DrmbService drmbService;
+	@Autowired
+	private FpclService fpclService;
+	@Autowired
+	private JyxxsqService JyxxsqService;
+	@Autowired
+	private PldrjlService pldrjlService;
+	@Autowired
+	private GetDataService getDataService;
+
+	@RequestMapping
+	@SystemControllerLog(description = "平台提取开票", key = "")
+	public String index() {
+		request.setAttribute("xfList", getXfList());
+		request.setAttribute("skpList", getSkpList());
+		List<Sm> list = smService.findAllByParams(new Sm());
+		request.setAttribute("smlist", list);
+		String gsdm = this.getGsdm();
+		List<Object> argList = new ArrayList<>();
+		argList.add(gsdm);
+		Cszb cszb = cszbService.getSpbmbbh(gsdm, getXfid(), null, "sfqyspz");
+		List<Spvo> list2 = new ArrayList<>();
+		if (null != cszb && cszb.getCsz().equals("是")) {
+			Map<String, Object> pMap = new HashMap<>();
+			pMap.put("xfs", getXfList());
+			list2 = spzService.findAllByParams(pMap);
+		}
+		if (list2.size() == 0) {
+			list2 = spvoService.findAllByGsdm(gsdm);
+		}
+
+		List<Xf> xfList = this.getXfList();
+		if (!list2.isEmpty()) {
+			request.setAttribute("sp", list2.get(0));
+		}
+		if (xfList.size() == 1) {
+			Map<String, Object> map = new HashMap<>();
+			/*
+			 * map.put("xfsh", xfList.get(0).getXfsh()); map.put("xfs",
+			 * getXfList());
+			 */
+			map.put("gsdm", gsdm);
+			List<Drmb> mbList = drmbService.findAllByParams(map);
+			Drmb mb = new Drmb();
+			mb.setXfsh(xfList.get(0).getXfsh());
+			request.setAttribute("skps", getSkpList());
+			request.setAttribute("skpSum", getSkpList().size());
+			request.setAttribute("mrmb", drmbService.findMrByParams(mb));
+			request.setAttribute("mbList", mbList);
+			request.setAttribute("mbSum", mbList.size());
+		}
+		if (xfList != null && xfList.size() > 0) {
+			request.setAttribute("xf", xfList.get(0));
+		}
+		request.setAttribute("spList", list2);
+		request.setAttribute("xfSum", xfList.size());
+
+		return "pttqkp/index";
+	}
+
+	@ResponseBody
+	@RequestMapping("/getItems")
+	public Map<String, Object> getItems(int length, int start, int draw, String jylsh, String jyrqq, String jyrqz,
+			String xfsh, boolean loaddata) throws Exception {
+		Map<String, Object> result = new HashMap<String, Object>();
+		if (loaddata) {
+			String gsdm = getGsdm();
+			Map map = getDataService.getldyxFirData(jylsh,gsdm);
+			String accessToken = map.get("accessToken").toString();
+			Map resultMap = getDataService.getldyxSecData(jylsh,gsdm,accessToken);
+			if(null == resultMap || 0 == resultMap.size() || (null !=resultMap.get("tmp")&&resultMap.get("tmp").equals(""))){
+				int total = 0;
+				result.put("recordsTotal", total);
+				result.put("recordsFiltered", total);
+				result.put("draw", draw);
+				result.put("data", new ArrayList<>());
+			}else{
+				List<Jyxxsq> jyxxsqList = (List<Jyxxsq>) resultMap.get("jyxxsqList");
+				result.put("recordsTotal", jyxxsqList.size());
+				result.put("recordsFiltered", jyxxsqList.size());
+				result.put("draw", draw);
+				result.put("data", jyxxsqList);
+			}
+		} else {
+			int total = 0;
+			result.put("recordsTotal", total);
+			result.put("recordsFiltered", total);
+			result.put("draw", draw);
+			result.put("data", new ArrayList<>());
+		}
+		return result;
+	}
+
+	@ResponseBody
+	@RequestMapping("/plkjcl")
+	public Map<String, Object> plkjcl(String jylsh, String xfid, String lrsj, String xh) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		Map<String, Object> params = new HashMap<String, Object>();
+		String gsdm = this.getGsdm();
+		params.put("xh", xh);
+		params.put("jylsh", jylsh);
+		params.put("lrsj", lrsj);
+		params.put("xfid", xfid);
+		params.put("gsdm", gsdm);
+		Pldrjl pljl = pldrjlService.findOneByParams(params);
+		pljl.setZtbz("1");
+		pldrjlService.save(pljl);
+		List<Jyxxsq> jyxxsqList = pldrjlService.findAllJyxxsqByParams(params);
+		try {
+			fpclService.zjkp(jyxxsqList, "01");
+			result.put("success", true);
+			result.put("msg", "数据已处理，请关注发票开具结果");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result.put("success", true);
+			result.put("msg", "数据已处理，请关注发票开具结果");
+		}
+		return result;
+	}
+
+	@ResponseBody
+	@RequestMapping("/plkjsh")
+	public Map<String, Object> plkjsh(String jylsh, String xfid, String lrsj, String xh) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		Map<String, Object> params = new HashMap<String, Object>();
+		String gsdm = this.getGsdm();
+		params.put("xh", xh);
+		params.put("jylsh", jylsh);
+		params.put("lrsj", lrsj);
+		params.put("xfid", xfid);
+		params.put("gsdm", gsdm);
+
+		try {
+			Pldrjl pljl = pldrjlService.findOneByParams(params);
+			pldrjlService.delete(pljl);
+
+			List<Jyxxsq> jyxxsqList = pldrjlService.findAllJyxxsqByParams(params);
+			List<Integer> sqlshList = new ArrayList<Integer>();
+			if (null != jyxxsqList && !jyxxsqList.isEmpty()) {
+				for (int i = 0; i < jyxxsqList.size(); i++) {
+					Jyxxsq jyxxsq = jyxxsqList.get(i);
+					sqlshList.add(jyxxsq.getSqlsh());
+				}
+			}
+			JyxxsqService.delBySqlshList2(sqlshList);
+			result.put("success", true);
+			result.put("msg", "删除成功");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result.put("success", false);
+			result.put("msg", "删除失败");
+		}
+		return result;
+	}
+
+}
