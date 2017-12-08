77f96e310d97ad20bb66da5159f6e6bc4c60f0e2
zhangsongqiang <zhangsongqiang@datarj.com>
2017-12-08 09:23:22 +0800

    去掉日志

--- src/main/java/com/rjxx/taxeasy/controller/PttqkpController.java
+++ src/main/java/com/rjxx/taxeasy/controller/PttqkpController.java
@@ -159,7 +159,7 @@ public class PttqkpController extends BaseController {
 		List<Jyxxsq> jyxxsqList = null;
 		List<Jymxsq>jymxsqList=null;
 		List<Jyzfmx>jyzfmxList=null;
-        logger.info("全家拉取数据之后的map"+JSON.toJSONString(resMap));
+        //logger.info("全家拉取数据之后的map"+JSON.toJSONString(resMap));
 		List list = new ArrayList();
 		if(resMap!=null){
 			jyxxsqList = (List<Jyxxsq>) resMap.get("jyxxsqList");
@@ -181,7 +181,7 @@ public class PttqkpController extends BaseController {
 					list.add(jyzfmxVo);
 			}
 		}
-		logger.info("---"+JSON.toJSONString(list));
+		//logger.info("---"+JSON.toJSONString(list));
 		if(jyxxsqList.size()>0){
 			resultMap.put("jyxxsq",jyxxsqList);
 			resultMap.put("jymxsq",jymxsqList);
