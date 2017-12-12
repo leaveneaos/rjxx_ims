94b6d70079343d1d7aaf60873039d8a1e944da2a
zhangsongqiang <zhangsongqiang@datarj.com>
2017-12-12 14:24:24 +0800

    发票重开

--- src/main/java/com/rjxx/taxeasy/controller/FpckController.java
+++ src/main/java/com/rjxx/taxeasy/controller/FpckController.java
@@ -5,21 +5,24 @@ import com.rjxx.taxeasy.bizcomm.utils.InvoiceResponse;
 import com.rjxx.taxeasy.bizcomm.utils.SkService;
 import com.rjxx.taxeasy.domains.*;
 import com.rjxx.taxeasy.filter.SystemControllerLog;
+import com.rjxx.taxeasy.invoice.DealOrder01;
+import com.rjxx.taxeasy.invoice.DealOrder04;
+import com.rjxx.taxeasy.invoice.Kphc;
 import com.rjxx.taxeasy.service.*;
 import com.rjxx.taxeasy.utils.UrlUtils;
 import com.rjxx.taxeasy.vo.Fpcxvo;
 import com.rjxx.taxeasy.web.BaseController;
 import com.rjxx.utils.HtmlUtils;
+import com.rjxx.utils.XmlUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by Administrator on 2017-12-05.
@@ -48,6 +51,19 @@ public class FpckController extends BaseController {
 
     @Autowired
     private KpspmxService kpspmxService;
+
+    @Autowired
+    private DealOrder04 dealOrder04;
+    @Autowired
+    private JylsService jylsService;
+    @Autowired
+    private DealOrder01 dealOrder01;
+    @Autowired
+    private JyxxsqService jyxxsqService;
+    @Autowired
+    private JymxsqService jymxsqService;
+    @Autowired
+    private JyzfmxService jyzfmxService;
 
 
 
@@ -191,7 +207,6 @@ public class FpckController extends BaseController {
      */
     @RequestMapping(value = "/fpckSave")
     @ResponseBody
-    @Transactional
     @SystemControllerLog(description = "发票重开",key = "kplshck")
     public Map fpck(String kplshck,String gfmcck,String gfshck,String gfemailck,
                     String gfdzck,String gfdhck,String gfyhck,String gfyhzhck,String sfbx) throws Exception {
@@ -206,29 +221,110 @@ public class FpckController extends BaseController {
             Kpls kpls=kplsService.findOne(Integer.parseInt(kplshck));
             if(kpls.getFpztdm().equals("00")){
                 try{
-                    /*Map paramsMap=new HashMap();
+                    Map paramsMap=new HashMap();
                     paramsMap.put("kplsh",kplshck);
                     List<Kpspmx> kpspmxList=  kpspmxService.findMxList(paramsMap);
-                    String ss="";
-                    String hcje="";
-                    for(int j=0;j<kpspmxList.size();j++){
-                        Kpspmx kpspmx=kpspmxList.get(j);
-                        ss+=kpspmx.getId()+",";
-                        hcje+=kpspmx.getKhcje()+",";
+
+                    Map jylsMap=new HashMap();
+                    jylsMap.put("jylsh",kpls.getJylsh());
+                    List<Jyls> list = jylsService.findAllByJylsh(jylsMap);
+
+
+                    List<Jyxxsq> jyxxsqList = new ArrayList();
+                    List<Jymxsq> jymxsqList = new ArrayList();
+                    List<Jyzfmx> jyzfmxList = new ArrayList<Jyzfmx>();
+
+                    Map jyxxsqMap = new HashMap();
+                    jyxxsqMap.put("jylsh",list.get(0).getJylsh());
+                    jyxxsqMap.put("gsdm",getGsdm());
+                    Jyxxsq jyxxsq = jyxxsqService.findOneByJylsh(jyxxsqMap);
+
+
+                    Map jymxsqMap = new HashMap();
+                    jymxsqMap.put("sqlsh",list.get(0).getSqlsh());
+                    jymxsqMap.put("gsdm",getGsdm());
+                    jymxsqList = jymxsqService.findAllBySqlsh(jymxsqMap);
+                    if(jymxsqList.size()<0){
+                        result.put("success", false);
+                        result.put("msg", "重新开具失败！");
+                        return result;
                     }
+                    Map jyzfsqMap = new HashMap();
+                    jyzfsqMap.put("gsdm",getGsdm());
+                    jyzfsqMap.put("sqlsh" , list.get(0).getSqlsh());
+                    jyzfmxList = jyzfmxService.findAllByParams(jyzfsqMap);
+//                    String ss="";
+//                    String hcje="";
+//                    for(int j=0;j<kpspmxList.size();j++){
+//                        Kpspmx kpspmx=kpspmxList.get(j);
+//                        ss+=kpspmx.getId()+",";
+//                        hcje+=kpspmx.getKhcje()+",";
+//                    }
                     //先红冲
-                    InvoiceResponse flag = fphcService.hccl(kpls.getKplsh(), getYhid(),
-                            getGsdm(), hcje, ss,kpls.getHztzdh(),kpls.getJylsh());
-                    if (flag.getReturnCode().equals("0000")) {
-                        result.put("success", true);
-                        result.put("msg", "重新开具成功！");
+                    //InvoiceResponse flag = fphcService.hccl(kpls.getKplsh(), getYhid(),
+//                            getGsdm(), hcje, ss,kpls.getHztzdh(),kpls.getJylsh());
+                    Kphc kphc = new Kphc();
+                    kphc.setCNDNCode(kpls.getFpdm());
+                    kphc.setCNDNNo(kpls.getFphm());
+                    kphc.setInvType(kpls.getFpzldm());
+                    kphc.setExtractCode(list.get(0).getTqm());
+                    kphc.setClientNO(jyxxsq.getKpddm());
+                    kphc.setServiceType("1");
+                    kphc.setSerialNumber("JY" + new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date()));
+                    kphc.setOrderNumber(list.get(0).getDdh());
+                    kphc.setCNNoticeNo(kpls.getHztzdh());
+                    BigDecimal big = new BigDecimal(kpls.getJshj().toString());
+                    BigDecimal totalAmount = big.multiply(new BigDecimal("-1"));
+                    kphc.setTotalAmount(Double.valueOf(totalAmount.toString()));
+                    Map hcMap= new HashMap();
+                    hcMap.put("Kphc",kphc);
+                    String resultxml = dealOrder04.execute(getGsdm(), hcMap, "04");
+                    Map<String, Object> resultHCMap = XmlUtil.xml2Map(resultxml);
+                    String returnHCMsg=resultHCMap.get("ReturnMessage").toString();
+                    String returnHCCode=resultHCMap.get("ReturnCode").toString();
+                    if (returnHCCode.equals("0000")) {
+                        if(null!=gfshck && !gfshck.equals("")){
+                            jyxxsq.setGflx("1");
+                        }else {
+                            jyxxsq.setGflx("0");
+                        }
+                        jyxxsq.setGfmc(gfmcck);
+                        jyxxsq.setGfsh(gfshck);
+                        jyxxsq.setGfemail(gfemailck);
+                        jyxxsq.setGfdz(gfdzck);
+                        jyxxsq.setGfdh(gfdhck);
+                        jyxxsq.setGfyh(gfyhck);
+                        jyxxsq.setGfyhzh(gfyhzhck);
+                        if(jymxsqList.get(0).getDdh()==null){
+                            jymxsqList.get(0).setDdh(jyxxsq.getDdh());
+                        }
+                        if(gfemailck!=null && !gfemailck.equals("")){
+                            jyxxsq.setSffsyj("1");
+                        }
+                        jyxxsq.setJylsh("JY" + new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date()));
+                        jyxxsqList.add(jyxxsq);
+                        Map kpMap = new HashMap();
+                        kpMap.put("jyxxsqList",jyxxsqList);
+                        kpMap.put("jymxsqList",jymxsqList);
+                        kpMap.put("jyzfmxList",jyzfmxList);
+                        String returnXml = dealOrder01.execute(getGsdm(), kpMap, "01");
+                        Map<String, Object> resultCKMap = XmlUtil.xml2Map(returnXml);
+                        String returnCKMsg=resultCKMap.get("ReturnMessage").toString();
+                        String returnCKCode=resultCKMap.get("ReturnCode").toString();
+                        if(returnCKCode.equals("0000")){
+                            result.put("success", true);
+                            result.put("msg", "重新开具成功！");
+                        }else {
+                            result.put("success", false);
+                            result.put("msg", "重新开具失败！"+returnCKMsg);
+                            return result;
+                        }
                     }else{
                         result.put("success", false);
-                        result.put("msg", "重新开具,红冲失败！"+flag.getReturnMessage());
+                        result.put("msg", "重新开具,红冲失败！"+returnHCMsg);
                         return result;
-                    }*/
-                    result.put("success", true);
-                    result.put("msg", "重新开具成功！");
+                    }
+
                 }catch (Exception e){
                     result.put("success", false);
                     result.put("msg", "重新开具失败！");
