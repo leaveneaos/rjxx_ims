b99fc834601bec36d4c27ed27de18d2231dce8ce
zhangsongqiang <zhangsongqiang@datarj.com>
2017-12-14 17:12:16 +0800

    添加文件上传大小限制

---
+++ src/main/java/com/rjxx/taxeasy/configuration/MultipartConfig.java
@@ -0,0 +1,27 @@
+package com.rjxx.taxeasy.configuration;
+
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.MultipartConfigElement;
+
+/**
+ * Created by Administrator on 2017-12-14.
+ */
+@Configuration
+public class MultipartConfig {
+    /**
+     * 文件上传配置
+     * @return
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //文件最大
+        factory.setMaxFileSize("10240KB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("102400KB");
+        return factory.createMultipartConfig();
+    }
+}
