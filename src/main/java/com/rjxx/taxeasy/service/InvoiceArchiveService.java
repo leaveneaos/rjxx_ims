39070fb2d883d8a8f72332ee7d8bc172008fc64c
wangyahui <24340912@qq.com>
2017-12-19 17:05:53 +0800

    获取归档下载路径更新为读文件获取，解决库中与服务器中不一致的问题

--- src/main/java/com/rjxx/taxeasy/service/InvoiceArchiveService.java
+++ src/main/java/com/rjxx/taxeasy/service/InvoiceArchiveService.java
@@ -1,10 +1,9 @@
 package com.rjxx.taxeasy.service;
 
-import com.rjxx.taxeasy.dao.KplsJpaDao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,18 +14,29 @@ import java.util.Map;
 public class InvoiceArchiveService {
 
     @Value("${gd_request_Path}")
-    private String path;
+    private String requestPath;
 
-    @Autowired
-    private KplsJpaDao kplsJpaDao;
+    @Value("${gd_zip_path}")
+    private String zipPath;
+
+    @Value("${gd_zip_path_day}")
+    private String zipPathDay;
 
     public Map getPDFPath(String date, String taxNo) {
         try {
-            Integer count = kplsJpaDao.countByLrsj(date, taxNo);
-            if (count == null || count == 0) {
-                return null;
+            String count = "";
+            File file = new File(zipPath + "/" + taxNo);
+            File[] dir = file.listFiles();
+            for (int i = 0; i < dir.length; i++) {
+                String name = dir[i].getName();
+                if (name.indexOf(taxNo + "-" + date) != -1) {
+                    int j = name.indexOf(".");
+                    int k = name.lastIndexOf("-");
+                    count = name.substring(k + 1, j);
+                    break;
+                }
             }
-            String pdfPath = path + "taxno/" + taxNo + "/" + taxNo + "-" + date + "-" + count + ".zip";
+            String pdfPath = requestPath + taxNo + "/" + taxNo + "-" + date + "-" + count + ".zip";
             Map<String, Object> map = new HashMap();
             map.put("path", pdfPath);
             map.put("count", count);
@@ -39,11 +49,19 @@ public class InvoiceArchiveService {
 
     public Map getPDFPath_Day(String date, String taxNo) {
         try {
-            Integer count = kplsJpaDao.countByLrsjDay(date, taxNo);
-            if (count == null || count == 0) {
-                return null;
+            String count = "";
+            File file = new File(zipPathDay + "/" + taxNo);
+            File[] dir = file.listFiles();
+            for (int i = 0; i < dir.length; i++) {
+                String name = dir[i].getName();
+                if (name.indexOf(taxNo + "-" + date) != -1) {
+                    int j = name.indexOf(".");
+                    int k = name.lastIndexOf("-");
+                    count = name.substring(k + 1, j);
+                    break;
+                }
             }
-            String pdfPath = path + "taxno/day/" + taxNo + "/" + taxNo + "-" + date + "-" + count + ".zip";
+            String pdfPath = requestPath + "day/" + taxNo + "/" + taxNo + "-" + date + "-" + count + ".zip";
             Map<String, Object> map = new HashMap();
             map.put("path", pdfPath);
             map.put("count", count);
