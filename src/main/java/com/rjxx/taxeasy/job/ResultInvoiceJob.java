c27069653643c3bec3f823e66111d2f541dabd1c
许黎明 <1084854604@qq.com>
2018-02-01 17:00:55 +0800

    no message

--- src/main/java/com/rjxx/taxeasy/job/ResultInvoiceJob.java
+++ src/main/java/com/rjxx/taxeasy/job/ResultInvoiceJob.java
@@ -8,6 +8,7 @@ import com.rjxx.taxeasy.domains.Jyls;
 import com.rjxx.taxeasy.domains.Kpls;
 import com.rjxx.taxeasy.service.*;
 import com.rjxx.taxeasy.vo.Kpspmxvo;
+import com.rjxx.time.TimeUtil;
 import com.rjxx.utils.DesUtils;
 import com.rjxx.utils.StringUtils;
 import com.rjxx.utils.XmlJaxbUtils;
@@ -26,6 +27,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.xml.sax.InputSource;
 
 import java.io.StringReader;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -84,7 +86,8 @@ public class ResultInvoiceJob  implements Job{
                         Kpls kpls = kplsService.findOneByParams(params);
                         if(kpls!=null){
                             if(kpls.getFpzldm().equals("12")){
-                                fpclService.updateKpls(resultMap);
+                               // fpclService.updateKpls(resultMap);
+                                this.updateDpresult(kpls,resultMap,suc,invoicedata);
                             }
                         }
                     }else if(invoicedata.contains("Response")){
@@ -97,6 +100,224 @@ public class ResultInvoiceJob  implements Job{
             e.printStackTrace();
         }
 
+    }
+    /**
+     * 更新交易流水状态
+     *
+     * @param djh
+     * @param clztdm
+     */
+    private void updateJyls(int djh, String clztdm) {
+        Jyls jyls = jylsService.findOne(djh);
+        if (jyls != null) {
+            jyls.setClztdm(clztdm);
+            jyls.setXgsj(new Date());
+            jylsService.save(jyls);
+        }
+    }
+    public void updateDpresult(Kpls kpls,Map resultMap,boolean suc,String content) throws Exception {
+        if (!suc) {
+            //解析xml异常
+            kpls.setFpztdm("05");
+            kpls.setErrorReason("返回的xml异常，无法解析");
+            kpls.setXgsj(new Date());
+            kplsService.save(kpls);
+            updateJyls(kpls.getDjh(), "92");
+            logger.error("dzfp return xml error!!!kplsh:" + kpls.getKplsh() + ",xml:" + content);
+            Map parms=new HashMap();
+            parms.put("gsdm",kpls.getGsdm());
+            Gsxx gsxx=gsxxService.findOneByParams(parms);
+            //String url="https://vrapi.fvt.tujia.com/Invoice/CallBack";
+            String url=gsxx.getCallbackurl();
+            if(!("").equals(url)&&url!=null){
+                String returnmessage=null;
+                if(!kpls.getGsdm().equals("Family")&&!kpls.getGsdm().equals("fwk")) {
+                    returnmessage = generatePdfService.CreateReturnMessage(kpls.getKplsh());
+                    //输出调用结果
+                    logger.info("回写报文" + returnmessage);
+                    if (returnmessage != null && !"".equals(returnmessage)) {
+                        Map returnMap = generatePdfService.httpPost(returnmessage, kpls);
+                        logger.info("返回报文" + JSON.toJSONString(returnMap));
+                    }
+                }else if(kpls.getGsdm().equals("fwk")){
+                    returnmessage = generatePdfService.CreateReturnMessage3(kpls.getKplsh());
+                    logger.info("回写报文" + returnmessage);
+                    if (returnmessage != null && !"".equals(returnmessage)) {
+                        String ss= HttpUtils.netWebService(url,"CallBack",returnmessage,gsxx.getAppKey(),gsxx.getSecretKey());
+                        logger.info("返回报文" + ss);
+                    }
+                }
+            }
+            return;
+        }
+        String dzfpReturnCode = (String) resultMap.get("RETURNCODE");
+        if (!"0000".equals(dzfpReturnCode)) {
+            //返回结果不正确
+            String dzfpReturnMsg = (String) resultMap.get("RETURNMSG");
+            if ("-99 流水号重复".equals(dzfpReturnMsg)) {
+                //返回重复结果
+                if (StringUtils.isNotBlank(kpls.getFphm())) {
+                    //状态正常，已经更新过了，不做处理了
+                    logger.warn("kplsh:" + kpls.getKplsh() + " -99 流水号重复");
+                    return;
+                }
+            }
+            kpls.setFpztdm("05");
+            if (StringUtils.isBlank(dzfpReturnMsg)) {
+                kpls.setErrorReason("未知异常，开票软件没有返回结果，请去开票软件确认");
+            } else {
+                kpls.setErrorReason(dzfpReturnMsg);
+            }
+            kplsService.save(kpls);
+            updateJyls(kpls.getDjh(), "92");
+            logger.error("dzfp return xml error!!!kplsh:" + kpls.getKplsh() + ",xml:" + content);
+            Map parms=new HashMap();
+            parms.put("gsdm",kpls.getGsdm());
+            Gsxx gsxx=gsxxService.findOneByParams(parms);
+            //String url="https://vrapi.fvt.tujia.com/Invoice/CallBack";
+            String url=gsxx.getCallbackurl();
+            if(!("").equals(url)&&url!=null){
+                String returnmessage=null;
+                if(!kpls.getGsdm().equals("Family")&&!kpls.getGsdm().equals("fwk")) {
+                    returnmessage = generatePdfService.CreateReturnMessage(kpls.getKplsh());
+                    //输出调用结果
+                    logger.info("回写报文" + returnmessage);
+                    if (returnmessage != null && !"".equals(returnmessage)) {
+                        Map returnMap = generatePdfService.httpPost(returnmessage, kpls);
+                        logger.info("返回报文" + JSON.toJSONString(returnMap));
+                    }
+                }else if(kpls.getGsdm().equals("fwk")){
+                    returnmessage = generatePdfService.CreateReturnMessage3(kpls.getKplsh());
+                    logger.info("回写报文" + returnmessage);
+                    if (returnmessage != null && !"".equals(returnmessage)) {
+                        String ss= HttpUtils.netWebService(url,"CallBack",returnmessage,gsxx.getAppKey(),gsxx.getSecretKey());
+                    }
+                }
+            }
+            return;
+        }
+        //保存正常结果
+        updateKpls(resultMap, kpls);
+        String czlxdm = kpls.getFpczlxdm();
+        if ("12".equals(czlxdm) || "13".equals(czlxdm)) {
+            updateJyls(kpls.getDjh(), "91");
+            if (kpls.getHzyfphm() != null && kpls.getHzyfpdm() != null) {
+                kpls.setJylsh("");
+                Kpls parms=new Kpls();
+                parms.setFpdm(kpls.getHzyfpdm());
+                parms.setFphm(kpls.getHzyfphm());
+                Kpls ykpls = kplsService.findByfphm(parms);
+                Map param2 = new HashMap<>();
+                param2.put("kplsh", ykpls.getKplsh());
+                // 全部红冲后修改
+                //Kpspmxvo mxvo = kpspmxService.findKhcje(param2);
+                //if (mxvo.getKhcje() == 0) {
+                param2.put("fpztdm", "02");
+                kplsService.updateFpczlx(param2);
+                       /* } else {
+                            param2.put("fpztdm", "01");
+                            kplsService.updateFpczlx(param2);
+                        }*/
+            }
+        } else {
+            updateJyls(kpls.getDjh(), "21");
+        }
+        //此处开始生成pdf
+
+        skService.ReCreatePdf(kpls.getKplsh());
+        Map parms=new HashMap();
+        parms.put("gsdm",kpls.getGsdm());
+        Gsxx gsxx=gsxxService.findOneByParams(parms);
+        //String url="https://vrapi.fvt.tujia.com/Invoice/CallBack";
+        String url=gsxx.getCallbackurl();
+        if(!("").equals(url)&&url!=null){
+            String returnmessage=null;
+            if(!kpls.getGsdm().equals("Family")&&!kpls.getGsdm().equals("fwk")) {
+                returnmessage = generatePdfService.CreateReturnMessage(kpls.getKplsh());
+                //输出调用结果
+                logger.info("回写报文" + returnmessage);
+                if (returnmessage != null && !"".equals(returnmessage)) {
+                    Map returnMap = generatePdfService.httpPost(returnmessage, kpls);
+                    logger.info("返回报文" + JSON.toJSONString(returnMap));
+                }
+            }else if(kpls.getGsdm().equals("fwk")){
+                returnmessage = generatePdfService.CreateReturnMessage3(kpls.getKplsh());
+                logger.info("回写报文" + returnmessage);
+                if (returnmessage != null && !"".equals(returnmessage)) {
+                    String ss= HttpUtils.netWebService(url,"CallBack",returnmessage,gsxx.getAppKey(),gsxx.getSecretKey());
+                    String fwkReturnMessageStr=fwkReturnMessage(kpls);
+                    logger.info("----------sap回写报文----------" + fwkReturnMessageStr);
+                    String Data= HttpUtils.doPostSoap1_2(gsxx.getSapcallbackurl(), fwkReturnMessageStr, null,"Wendy","Welcome9");
+                    logger.info("----------fwk平台回写返回报文--------" + ss);
+                    logger.info("----------sap回写返回报文----------" + Data);
+
+                }
+            }
+        }
+    }
+    public String   fwkReturnMessage(Kpls kpls) {
+        SimpleDateFormat sim = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        String result="Succeed";
+        if(kpls.getFpczlxdm().equals("12")){
+            result="CancelSucceed";
+        }
+        String ss="\n" +
+                "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:glob=\"http://sap.com/xi/SAPGlobal20/Global\">\n" +
+                "   <soap:Header/>\n" +
+                "   <soap:Body>\n" +
+                "      <glob:GoldenTaxGoldenTaxCreateRequest_sync>\n" +
+                "         <BasicMessageHeader></BasicMessageHeader>\n" +
+                "         <GoldenTax>\n" +
+                "            <CutInvID>"+kpls.getJylsh()+"</CutInvID>\n" +
+                "            <GoldenTaxID>"+kpls.getFphm()+"</GoldenTaxID>\n" +
+                "            <GoldenTaxDate>\n" +
+                "               <StartDateTime>"+sim.format(kpls.getKprq())+"</StartDateTime>\n" +
+                "               <EndDateTime>"+sim.format(kpls.getKprq())+"</EndDateTime>\n" +
+                "            </GoldenTaxDate>\n" +
+                "            <GoldenTaxResult>"+result+"</GoldenTaxResult>\n" +
+                "            <GoldenTaxCode>"+kpls.getFpdm()+"</GoldenTaxCode>\n" +
+                "         </GoldenTax>\n" +
+                "      </glob:GoldenTaxGoldenTaxCreateRequest_sync>\n" +
+                "   </soap:Body>\n" +
+                "</soap:Envelope>";
+        return ss;
+    }
+    /**
+     * 在t_kpls及t_kpspmx 表中增加记录
+     *
+     * @param map
+     * @param kpls
+     */
+    private void updateKpls(Map<String, String> map, Kpls kpls) {
+        // 保存已开发票结果主表
+        try {
+            String fpdm = map.get("FP_DM");
+            String fphm = map.get("FP_HM");
+            String czlx = kpls.getFpczlxdm();
+            kpls.setFpdm(fpdm);
+            kpls.setFphm(fphm);
+            if ("13".equals(czlx)) {
+                kpls.setHkbz("1");
+            } else {
+                kpls.setHkbz("0");
+            }
+            kpls.setFpEwm(map.get("EWM"));
+            kpls.setSksbm(map.get("JQBH"));
+            kpls.setMwq(map.get("FP_MW"));
+            kpls.setJym(map.get("JYM"));
+            String kprq = map.get("KPRQ");
+            kpls.setKprq(TimeUtil.getSysDateInDate(kprq, null));
+            String bz = map.get("BZ");
+            if (StringUtils.isNotBlank(bz)) {
+                kpls.setBz(bz);
+            }
+            kpls.setFpztdm("00");
+            kpls.setErrorReason(null);
+            kpls.setXgsj(new Date());
+            kplsService.save(kpls);
+        } catch (Exception e) {
+            logger.error("", e);
+        }
     }
     /**
      * 更新开票结果
