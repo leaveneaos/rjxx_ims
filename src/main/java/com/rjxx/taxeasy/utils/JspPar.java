b98072c4ddab0e5ed9356f74679542dfa1027b7a
chenlong <345048305@qq.com>
2018-08-27 16:59:02 +0800

    新增select标签需要的参数集，与input标签共享，使所有输入框看起来风格比较一致

--- src/main/java/com/rjxx/taxeasy/utils/JspPar.java
+++ src/main/java/com/rjxx/taxeasy/utils/JspPar.java
@@ -1,15 +1,29 @@
 package com.rjxx.taxeasy.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JspPar {
     private JspPar(){
         this.btMarginLeft=15;
         this.btText="查询";
+        this.select=new HashMap<String,String>();
+        this.select.put("height","33px");
     }
 
     private Integer btMarginLeft;
     private String btText;
+    private Map<String,String> select;
 
     private static JspPar par;
+
+    public Map<String, String> getSelect() {
+        return select;
+    }
+
+    public void setSelect(Map<String, String> select) {
+        this.select = select;
+    }
 
     public static JspPar getPar(){
         if(par==null){
